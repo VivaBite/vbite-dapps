859ee9932aeba2f2327b99a4a7c46dcc6bb6c564
Serhii Mykhailovskyi <sergio@smounters.com>
2025-06-01 16:44:28 -0400

    plans and features

---
+++ contracts/VBITEAirdrop.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/access/Ownable.sol";
+import "@openzeppelin/token/ERC20/ERC20.sol";
+import "@openzeppelin/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/utils/Pausable.sol";
+
+/**
+ * @title VBITE Airdrop contract
+ */
